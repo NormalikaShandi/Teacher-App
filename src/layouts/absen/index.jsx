485956574e6e9a96ddb04f78fcab63eace9ef991
Normalika Shandi <nor.malika.sh@gmail.com>
2022-12-28 10:25:26 +0700

    first commit

---
+++ src/layouts/absen/index.jsx
@@ -0,0 +1,81 @@
+/**
+=========================================================
+* Material Dashboard 2 React - v2.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// @mui material components
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+
+// Material Dashboard 2 React components
+import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
+
+// Material Dashboard 2 React example components
+import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+import Footer from "examples/Footer";
+import DataTable from "examples/Tables/DataTable";
+
+// Data
+import absenTableData from "layouts/absen/data/absenTableData";
+import MDButton from "components/MDButton";
+import { Icon } from "@mui/material";
+
+function Tables() {
+  const { columns: pColumns, rows: pRows } = absenTableData();
+  return (
+    <DashboardLayout>
+      <DashboardNavbar />
+      <MDBox pt={6} pb={3}>
+        <Grid container spacing={6}>
+          <Grid item xs={12}>
+            <Card>
+              <MDBox
+                py={3}
+                px={2}
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                variant="gradient"
+                bgColor="info"
+                borderRadius="lg"
+                coloredShadow="info"
+              >
+                <MDTypography variant="h6" color="white">
+                  Absen
+                </MDTypography>
+                <MDButton variant="gradient" color="dark">
+                  <Icon sx={{ fontWeight: "bold" }}>add</Icon>
+                  &nbsp;tambah
+                </MDButton>
+              </MDBox>
+              <MDBox pt={3}>
+                <DataTable
+                  table={{ columns: pColumns, rows: pRows }}
+                  isSorted={false}
+                  entriesPerPage={false}
+                  showTotalEntries={false}
+                  noEndBorder
+                />
+              </MDBox>
+            </Card>
+          </Grid>
+        </Grid>
+      </MDBox>
+      <Footer />
+    </DashboardLayout>
+  );
+}
+
+export default Tables;
---
+++ src/layouts/siswa/data/siswaTableData.jsx
@@ -0,0 +1,73 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/function-component-definition */
+/**
+=========================================================
+* Material Dashboard 2 React - v2.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// @mui material components
+
+// Material Dashboard 2 React components
+import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
+import MDAvatar from "components/MDAvatar";
+import { Link } from "react-router-dom";
+
+export default function data() {
+  const Project = ({ image, name }) => (
+    <MDBox display="flex" alignItems="center" lineHeight={1}>
+      <MDAvatar src={image} name={name} size="sm" variant="rounded" />
+      <MDTypography display="block" variant="button" fontWeight="medium" ml={1} lineHeight={1}>
+        {name}
+      </MDTypography>
+    </MDBox>
+  );
+
+  return {
+    columns: [
+      { Header: "nama siswa", accessor: "nama", align: "left" },
+      { Header: "absen", accessor: "absen", width: "20%", align: "left" },
+      { Header: "nilai", accessor: "nilai", width: "20%", align: "left" },
+    ],
+
+    rows: [
+      {
+        nama: <Project name="Asana" />,
+        absen: (
+          <MDTypography
+            component={Link}
+            to="/absen"
+            variant="button"
+            color="info"
+            fontWeight="medium"
+            textGradient
+          >
+            Lihat
+          </MDTypography>
+        ),
+        nilai: (
+          <MDTypography
+            component={Link}
+            to="/nilai"
+            variant="button"
+            color="info"
+            fontWeight="medium"
+            textGradient
+          >
+            Lihat
+          </MDTypography>
+        ),
+      },
+    ],
+  };
+}
